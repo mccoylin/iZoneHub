6d3fddfe8bacb339f873a89ece5478f79e1afc18
mccoy <mccoy.lin@gmail.com>
2025-07-25 21:50:01 +0800

    把專案加進 main 裡。

---
+++ src/main/java/com/iZoneHub/demo/IZoneHubApplication.java
@@ -0,0 +1,15 @@
+package com.iZoneHub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IZoneHubApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(IZoneHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iZoneHub/demo/IZoneHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iZoneHub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IZoneHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
